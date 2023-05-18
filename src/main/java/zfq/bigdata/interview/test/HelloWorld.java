6e734a5afe89d7a21de5411f61880a13c5ef1de0
zfq314 <zfq0314@163.com>
2023-05-18 14:01:34 +0800

    大数据面试初次提交

---
+++ src/main/java/zfq/bigdata/interview/test/HelloWorld.java
@@ -0,0 +1,14 @@
+package zfq.bigdata.interview.test;
+
+/**
+ * @ClassName HelloWorld
+ * @Description TODO
+ * @Author ZFQ
+ * @Date 2023/5/18 13:58
+ * @Version 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
