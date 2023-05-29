de82bb3cfbb432398772e475a72a8cd7dde5d022
zfq314 <zfq0314@163.com>
2023-05-29 16:02:19 +0800

    输出到MySQL（JDBC）

---
+++ flink1.17.0/src/main/java/zfq/bigdata/interview/flink/chapter5/SinkMySQL.java
@@ -0,0 +1,65 @@
+package zfq.bigdata.interview.flink.chapter5;
+
+import org.apache.flink.connector.jdbc.JdbcConnectionOptions;
+import org.apache.flink.connector.jdbc.JdbcExecutionOptions;
+import org.apache.flink.connector.jdbc.JdbcSink;
+import org.apache.flink.connector.jdbc.JdbcStatementBuilder;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.api.functions.sink.SinkFunction;
+
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+/**
+ * @ClassName SinkMySQL
+ * @Description TODO 输出数据到MySQL
+ * @Author ZFQ
+ * @Date 2023/5/29 15:19
+ * @Version 1.0
+ */
+public class SinkMySQL {
+    public static void main(String[] args) throws Exception {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+        env.setParallelism(1);
+
+        SingleOutputStreamOperator<WaterSensor> sensorDs = env.socketTextStream("hadoop31", 7777)
+                .map(new WaterSensorMapFunction());
+        sensorDs.print();
+
+        /**
+         * TODO 写入mysql
+         * 1、只能用老的sink写法： addsink
+         * 2、JDBCSink的4个参数:
+         *    第一个参数： 执行的sql，一般就是 insert into
+         *    第二个参数： 预编译sql， 对占位符填充值
+         *    第三个参数： 执行选项 ---》 攒批、重试
+         *    第四个参数： 连接选项 ---》 url、用户名、密码
+         */
+        SinkFunction<WaterSensor> jdbcSink = JdbcSink.sink(
+                "insert into ws values(?,?,?)",
+                new JdbcStatementBuilder<WaterSensor>() {
+                    @Override
+                    public void accept(PreparedStatement preparedStatement, WaterSensor waterSensor) throws SQLException {
+                        //每收到一条WaterSensor，如何去填充占位符
+                        preparedStatement.setString(1, waterSensor.getId());
+                        preparedStatement.setLong(2, waterSensor.getTs());
+                        preparedStatement.setInt(3, waterSensor.getVc());
+                    }
+                },
+                JdbcExecutionOptions.builder()
+                        .withMaxRetries(3)//重试次数
+                        .withBatchSize(10)// 重试批次的大小条数
+                        .withBatchIntervalMs(300)//批次的时间
+                        .build(),
+                new JdbcConnectionOptions.JdbcConnectionOptionsBuilder()
+                        .withUrl("jdbc:mysql://hadoop31:3306/mybatis?serverTimezone=Asia/Shanghai&useUnicode=true&characterEncoding=UTF-8")
+                        .withUsername("root")
+                        .withPassword("hadoopdb-hadooponeoneone@dc.com.")
+                        .withConnectionCheckTimeoutSeconds(60)
+                        .build()
+        );
+        sensorDs.addSink(jdbcSink);
+        env.execute();
+    }
+}
