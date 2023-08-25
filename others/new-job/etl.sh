b4423c12217dd267e99e8e2a7974480bb78c0905
zfq314 <zfq0314@163.com>
2023-08-25 16:54:28 +0800

    业务提交

---
+++ others/new-job/etl.sh
@@ -0,0 +1,4 @@
+熟悉etl流程
+			Extract-Transform-Load
+
+sql编程经验
---
+++ others/new-job/实时数仓.sh
@@ -0,0 +1,22 @@
+SeaTunnel
+flink
+kafka
+ 
+
+阿里的产品
+		Dataworks 
+				 DataWorks（数据工场，原大数据开发套件）是阿里云重要的PaaS平台产品，提供数据集成、数据开发、数据地图、数据质量和数据服务等全方位的产品服务，一站式开发管理的界面，帮助企业专注于数据价值的挖掘和探索。DataWorks支持多种计算和存储引擎服务，包括离线计算MaxCompute、开源大数据引擎E-MapReduce、实时计算（基于Flink）、机器学习PAI、图计算服务GraphCompute和交互式分析服务等，并且支持用户自定义接入计算和存储服务。DataWorks为您提供全链路智能大数据及AI开发和治理服务。
+
+		MaxComputer
+					MaxCompute是适用于数据分析场景的企业级SaaS（Software as a Service）模式云数据仓库，以Serverless架构提供快速、全托管的在线数据仓库服务，消除了传统数据平台在资源扩展性和弹性方面的限制，最小化用户运维投入，使您可以经济并高效地分析处理海量数据。
+
+		Hologres
+				Hologres是阿里巴巴自主研发的一站式实时数仓引擎（Real-Time Data Warehouse），支持海量数据实时写入、实时更新、实时加工、实时分析，支持标准SQL（兼容PostgreSQL协议和语法，支持大部分PostgreSQL函数），支持PB级数据多维分析（OLAP）与即席分析（Ad Hoc），支持高并发低延迟的在线数据服务（Serving），支持多种负载的细粒度隔离与企业级安全能力，与MaxCompute、Flink、DataWorks深度融合，提供企业级离在线一体化全栈数仓解决方案。
+
+
+Hadoop生态体系
+
+Hadoop/HDFS/Hbase/Flink/Spark/kafka/clickhouse/mysql/MongoDB
+
+
+
