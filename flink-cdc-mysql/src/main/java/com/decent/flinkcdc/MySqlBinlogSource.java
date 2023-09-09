a384aebff2bd21e60f6dffaca195e1775bd8a1b7
zfq314 <zfq0314@163.com>
2023-09-09 18:25:03 +0800

    flinkcdc内容更新

--- flink-cdc-mysql/src/main/java/com/decent/flinkcdc/MySqlBinlogSource.java
+++ flink-cdc-mysql/src/main/java/com/decent/flinkcdc/MySqlBinlogSource.java
@@ -1,7 +1,6 @@
 package com.decent.flinkcdc;
 
 import com.ververica.cdc.connectors.mysql.MySqlSource;
-import com.ververica.cdc.debezium.StringDebeziumDeserializationSchema;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.functions.source.SourceFunction;
