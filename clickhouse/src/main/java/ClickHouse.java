27cadf7bb1e1872fdfa199caeeeac42ed631cdf8
zfq314 <zfq0314@163.com>
2023-11-07 16:59:44 +0800

    新增clickhouse模块

---
+++ clickhouse/src/main/java/ClickHouse.java
@@ -0,0 +1,12 @@
+/**
+ * @ClassName ClickHouse
+ * @Description TODO
+ * @Author ZFQ
+ * @Date 2023/11/7 16:58
+ * @Version 1.0
+ */
+public class ClickHouse {
+    public static void main(String[] args) {
+        System.out.println(ClassLoader.class.getClass().getName());
+    }
+}
